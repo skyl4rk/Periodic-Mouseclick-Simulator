841aa2ce659d5e9c1628d7cf88d7092b0201ffe6
skyl4rk <skyl4rk@gmail.com>
2022-08-27 16:27:18 -0400

    moved to time.time() control

--- periodic_mouseclick_simulator.py
+++ periodic_mouseclick_simulator.py
@@ -4,8 +4,9 @@
 # Requirements: pynput, pyautogui
 
 # Begin User Definition Area
-
-period = 60 # number of minutes between mouse clicks
+ENABLE_MOUSE_CLICK_SIMULATOR = True
+mouse_click_period = 1 # Number of minutes between mouse clicks
+#period = 60 # number of minutes between mouse clicks
 
 # End User Definition Area
  
@@ -24,20 +25,24 @@ def on_click(x, y, button, pressed):
 # Declare global variables
 xvar = 1
 yvar = 1
+previous_mouse_click_time = time.time()
 
+
+if ENABLE_MOUSE_CLICK_SIMULATOR:
 # Display alert screen
-pg.alert('Click OK, then click the desired mouse click location on the screen.')
+    pg.alert('Click OK, then click the desired mouse click location on the screen.')
 
 # Start listener for mouse clicks
-listener = mouse.Listener(on_click=on_click)
-listener.start()
-listener.join()
+    listener = mouse.Listener(on_click=on_click)
+    listener.start()
+    listener.join()
 
 # Start mouse click timer operation
-while True:
-    pg.click(xvar, yvar)
-    t = time.localtime()
-    current_time = time.strftime("%c", t)
-    print('Mouse click at:', current_time)
-    print('Next mouse click in '+ str(period) + ' minute(s).')
-    time.sleep(period * 60)
+    while True:
+        if time.time() > (mouse_click_period * 60) + previous_mouse_click_time:
+            pg.click(xvar, yvar)
+            t = time.localtime()
+            current_time = time.strftime("%c", t)
+            print('Mouse click at:', current_time)
+            print('Next mouse click in '+ str(mouse_click_period) + ' minute(s).')
+            previous_mouse_click_time = time.time()
